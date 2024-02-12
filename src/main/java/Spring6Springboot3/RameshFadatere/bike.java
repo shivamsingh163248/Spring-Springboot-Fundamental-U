32c943187ac76620cc0b2c11c3b6c6cbae288c06
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-13 03:06:13 +0530

    upate

---
+++ src/main/java/Spring6Springboot3/RameshFadatere/bike.java
@@ -0,0 +1,9 @@
+package Spring6Springboot3.RameshFadatere;
+
+public class bike {
+
+    // creating the method of the move
+    public void move(){
+        System.out.println("bike is moveing .....");
+    }
+}
---
+++ src/main/java/spring/loseCoupling/vehivals.java
@@ -0,0 +1,5 @@
+package spring.loseCoupling;
+
+public interface vehivals {
+
+}
