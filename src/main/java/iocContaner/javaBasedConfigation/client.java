6c4b68ae69e786d074328d24ade064513750256f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-14 22:55:14 +0530

    update

---
+++ src/main/java/iocContaner/javaBasedConfigation/client.java
@@ -0,0 +1,19 @@
+package iocContaner.javaBasedConfigation;
+
+public class client {
+
+    // creating the object of the vehical
+    public static void main(String[] args) {
+        vehicals vehicals = new car() ;
+        vehicals.moving();
+        // if i am calling the traveler method then it wll more flexible
+
+        //-------------------------------------------------------------
+
+        traveler traveler = new traveler(vehicals) ;
+        // traveler object using the method start journey
+        traveler.startJourny();
+
+
+    }
+}
