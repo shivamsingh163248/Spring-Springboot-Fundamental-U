33693cf3779f7d26fb41dfec28d1e0eba7e4844c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-17 02:08:23 +0530

    learning the ioc container

---
+++ src/main/java/iocContainer/javaBasedConfigation/Classes.java
@@ -0,0 +1,13 @@
+package iocContainer.javaBasedConfigation;
+
+public interface Classes {
+
+    // this is the interface of the class
+    // implement the following method
+
+
+    public String classTeacher() ;
+    public int  className() ;
+
+    public void studentCaption() ;
+}
---
+++ src/main/java/iocContainer/javaBasedConfigation/FistClass.java
@@ -0,0 +1,21 @@
+package iocContainer.javaBasedConfigation;
+
+public class FistClass implements Classes {
+
+
+
+    @Override
+    public String classTeacher() {
+        return "sakshiMishra";
+    }
+
+    @Override
+    public int className() {
+        return 1;
+    }
+
+    @Override
+    public void studentCaption() {
+        System.out.println("this is the class fist");
+    }
+}
