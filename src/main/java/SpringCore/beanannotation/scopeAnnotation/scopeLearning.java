e8f12e1ee18395556b706a0e33626070edb45309
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-11 20:02:36 +0530

    update

---
+++ src/main/java/SpringCore/beanannotation/scopeAnnotation/scopeLearning.java
@@ -0,0 +1,16 @@
+package SpringCore.beanannotation.scopeAnnotation;
+
+public class scopeLearning {
+
+    // learning scope of beans in the java
+    // singleton
+    // prototype
+    // request
+    // application
+    // websocket
+
+    // the last four scope mentioned , request , session ,application and websocket
+
+
+
+}
