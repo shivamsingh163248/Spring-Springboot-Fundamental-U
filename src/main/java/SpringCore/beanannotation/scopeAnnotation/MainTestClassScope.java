ed989512b5036469c71036f2c3458ddecb6702b5
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-11 22:47:42 +0530

    learning the scope  prototye and sigleton @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)

--- src/main/java/SpringCore/beanannotation/scopeAnnotation/MainTestClassScope.java
+++ src/main/java/SpringCore/beanannotation/scopeAnnotation/MainTestClassScope.java
@@ -1,4 +1,42 @@
 package SpringCore.beanannotation.scopeAnnotation;
 
+    import com.sun.security.auth.login.ConfigFile;
+    import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.swing.*;
+
 public class MainTestClassScope {
+
+    // creatng the main method
+    public static void main(String[] args) {
+
+        // creating the try and creating the application context
+      ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class) ;
+
+        // now using the getbean method to getting the class in the bean
+        SingltonBean singltonBean = applicationContext.getBean(SingltonBean.class) ;
+        // now creating the instace of the singeton bean
+  SingltonBean singltonBean1 = applicationContext.getBean(SingltonBean.class) ;
+       SingltonBean singltonBean2 = applicationContext.getBean(SingltonBean.class) ;
+
+       // now testing the prototype bean
+//      beanDefault aDefault1 = applicationContext.getBean(beanDefault.class) ;
+//     beanDefault aDefault2 = applicationContext.getBean(beanDefault.class) ;
+//       beanDefault aDefault3 = applicationContext.getBean(beanDefault.class) ;
+
+        // now creating the prototype
+
+        PrototypeBean prototypeBean1 = applicationContext.getBean(PrototypeBean.class) ;
+        PrototypeBean prototypeBean2 = applicationContext.getBean(PrototypeBean.class) ;
+        PrototypeBean prototypeBean3 = applicationContext.getBean(PrototypeBean.class) ;
+
+
+
+
+
+
+
+
+    }
 }
--- src/main/java/SpringCore/beanannotation/scopeAnnotation/beanDefault.java
+++ src/main/java/SpringCore/beanannotation/scopeAnnotation/beanDefault.java
@@ -1,14 +1,17 @@
 package SpringCore.beanannotation.scopeAnnotation;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SingltonbeanDefault {
+
+public class beanDefault {
 
     // now learning the in the case of the Singleton bean at the default
     // creating the constructor of the default singltan
-    public  SingltonbeanDefault(){
-        System.out.println("this is the default singleton value ");
+    public beanDefault(){
+        System.out.println("this is the default  value ");
     }
 
 }
