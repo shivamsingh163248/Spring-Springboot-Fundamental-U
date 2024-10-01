1c30f2dfcd2a33f1a01314e8d257db96138553b5
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-01 23:59:53 +0530

    learning the ioc container

--- src/main/java/learningsSpring/Ramesh/Basic/RunApplication.java
+++ src/main/java/learningsSpring/Ramesh/Basic/RunApplication.java
@@ -3,7 +3,11 @@ package learningsSpring.Ramesh.Basic;
 public class RunApplication {
     public static void main(String[] args) {
 
+        Delhivery shivam = new Delhivery() ;
 
+        Apple apple = new Apple() ;
+
+        shivam.allDetails(apple);
 
     }
 }
