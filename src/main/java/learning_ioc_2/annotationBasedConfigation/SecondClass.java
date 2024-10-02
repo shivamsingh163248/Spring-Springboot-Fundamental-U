18e6c70ddf31053002121f0a72116570c259d7ad
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-02 23:20:07 +0530

    updating the values

--- src/main/java/learning_ioc_2/annotationBasedConfigation/SecondClass.java
+++ src/main/java/learning_ioc_2/annotationBasedConfigation/SecondClass.java
@@ -1,4 +1,20 @@
 package learning_ioc_2.annotationBasedConfigation;
 
-public class SecondClass {
+public class SecondClass implements SchoolClass{
+
+    @Override
+    public void className() {
+        System.out.println("class name is 2");
+    }
+
+    @Override
+    public void classTeacher() {
+        System.out.println("class teacher name itisha singh ");
+    }
+
+    @Override
+    public void classDetails() {
+        System.out.println("this is the 2rd class   all the details ");
+
+    }
 }
--- src/main/java/learning_ioc_2/annotationBasedConfigation/firstClass.java
+++ src/main/java/learning_ioc_2/annotationBasedConfigation/firstClass.java
@@ -1,4 +1,21 @@
 package learning_ioc_2.annotationBasedConfigation;
 
-public class firstClass {
+public class firstClass implements  SchoolClass{
+
+    @Override
+    public void className() {
+        System.out.println("class name is 1");
+    }
+
+    @Override
+    public void classTeacher() {
+        System.out.println("class teacher name love you ");
+    }
+
+    @Override
+    public void classDetails() {
+        System.out.println("this is the 1rd class   all the details ");
+
+    }
+
 }
