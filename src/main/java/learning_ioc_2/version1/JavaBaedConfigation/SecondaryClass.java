775bec976e352576f12cfe435f367d54fd6ba978
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-02 13:38:36 +0530

    learnig the java based configation and the dependency injection and learnin the lose coupling

--- src/main/java/learning_ioc_2/version1/JavaBaedConfigation/SecondaryClass.java
+++ src/main/java/learning_ioc_2/version1/JavaBaedConfigation/SecondaryClass.java
@@ -5,10 +5,15 @@ public class SecondaryClass implements schoolClass {
     @Override
     public void classFeature() {
 
+        System.out.print("this is the secondary class apply for the 9-10");
+
     }
 
     @Override
     public void classDetails() {
+
+        System.out.print("this is the  clas 9 ");
+        System.out.print("this is the  clas 10");
 
     }
 }
