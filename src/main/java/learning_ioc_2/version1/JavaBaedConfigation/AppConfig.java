9f7660a6566bbe2c306280f30c98ad38c6c7b4cf
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-02 19:48:22 +0530

    updating the values

--- src/main/java/learning_ioc_2/version1/JavaBaedConfigation/AppConfig.java
+++ src/main/java/learning_ioc_2/version1/JavaBaedConfigation/AppConfig.java
@@ -1,7 +1,25 @@
 package learning_ioc_2.version1.JavaBaedConfigation;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    // creating the bean of the all the class
+    @Bean
+    public schoolClass primary(){
+        return new primaryClass() ;
+    }
+
+    @Bean
+    public schoolClass seSchoolClass(){
+        return new SecondaryClass() ;
+    }
+
+    // creating the bean
+    @Bean
+    public School school(){
+        return new School(primary()) ;
+    }
 }
