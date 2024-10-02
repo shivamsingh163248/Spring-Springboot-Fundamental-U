e02d9b8b9c50fab85954c3d6ca687f683c962037
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-02 23:18:16 +0530

    updating the values

--- src/main/java/learning_ioc_2/annotationBasedConfigation/ThirdClass.java
+++ src/main/java/learning_ioc_2/annotationBasedConfigation/ThirdClass.java
@@ -5,16 +5,17 @@ public class ThirdClass implements SchoolClass {
 
     @Override
     public void className() {
-
+        System.out.println("class name is 3");
     }
 
     @Override
     public void classTeacher() {
-
+         System.out.println("class teacher name shivam singh ");
     }
 
     @Override
     public void classDetails() {
+        System.out.println("this is the 3rd class   all the details ");
 
     }
 }
