5e3c63da8ed6f530d1730804bd7e84e31aeca318
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-03 19:36:29 +0530

    learning the dependecy injection

---
+++ src/main/java/learning_ioc_2/LouseCouplingAchiveUsingInterface/MessageSander.java
@@ -0,0 +1,19 @@
+package learning_ioc_2.LouseCouplingAchiveUsingInterface;
+
+public class MessageSander {
+
+// creating the instace
+    ServiceType type ;
+
+    // creating the constructor
+
+    public MessageSander(ServiceType type){
+
+        this.type = type  ;
+    }
+
+    // creating the method
+    public void SandMessage(){
+
+    }
+}
