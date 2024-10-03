f0e84d2119835ca68cce1f8f4f08df115d69fd79
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-03 11:26:35 +0530

    learning the Repository annotation and controller and service annotation

---
+++ src/main/java/learning_ioc_2/SteryTypeControllerRepositoyService/Reposetory/RepositoryClass.java
@@ -0,0 +1,9 @@
+package learning_ioc_2.SteryTypeControllerRepositoyService.Reposetory;
+
+public class RepositoryClass {
+
+    // creating the method of the Repository method
+    public String ReposString(){
+        return "this is the Repository class" ;
+    }
+}
---
+++ src/main/java/learning_ioc_2/SteryTypeControllerRepositoyService/contoler/ControllerClass.java
@@ -0,0 +1,11 @@
+package learning_ioc_2.SteryTypeControllerRepositoyService.contoler;
+
+import com.sun.source.tree.ReturnTree;
+
+public class ControllerClass {
+
+    // creating the method
+    public String ControllerStatement(){
+        return "this is the controller Statement" ;
+    }
+}
