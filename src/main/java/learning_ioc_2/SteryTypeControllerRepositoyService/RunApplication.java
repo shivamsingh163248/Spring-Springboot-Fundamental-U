185d317632c405e7b2ab038156de20f1fb8d534d
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-03 11:35:54 +0530

    learning the all the

---
+++ src/main/java/learning_ioc_2/SteryTypeControllerRepositoyService/RunApplication.java
@@ -0,0 +1,7 @@
+package learning_ioc_2.SteryTypeControllerRepositoyService;
+
+public class RunApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/learning_ioc_2/SteryTypeControllerRepositoyService/service/ServiceClass.java
@@ -0,0 +1,9 @@
+package learning_ioc_2.SteryTypeControllerRepositoyService.service;
+
+public class ServiceClass {
+
+    // creating the service method for the return the string of the service
+    public  String ServiceString(){
+        return "this is the service string " ;
+    }
+}
