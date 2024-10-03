bced031f1a3960520da3aa0966a85042c1db4d03
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-03 15:06:02 +0530

    updating and learning type of dipency injection

--- src/main/java/learning_ioc_2/LouseCouplingAchiveUsingInterface/ServiceType.java
+++ src/main/java/learning_ioc_2/LouseCouplingAchiveUsingInterface/ServiceType.java
@@ -2,4 +2,8 @@ package learning_ioc_2.LouseCouplingAchiveUsingInterface;
 
 public interface ServiceType {
 
+    // creating the function
+    public void sandMessage(String message) ;
+    // this is the function implement
+
 }
---
+++ src/main/java/learning_ioc_2/LouseCouplingAchiveUsingInterface/WhatsAppService.java
@@ -0,0 +1,11 @@
+package learning_ioc_2.LouseCouplingAchiveUsingInterface;
+
+public class WhatsAppService implements ServiceType {
+
+
+
+    @Override
+    public void sandMessage(String message) {
+
+    }
+}
