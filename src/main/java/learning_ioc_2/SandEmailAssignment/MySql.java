8cdd4304d272b802a2100e8b90258a95e1a5f1e1
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-05 02:14:29 +0530

    updating the mysql file

--- src/main/java/learning_ioc_2/SandEmailAssignment/MySql.java
+++ src/main/java/learning_ioc_2/SandEmailAssignment/MySql.java
@@ -1,4 +1,13 @@
 package learning_ioc_2.SandEmailAssignment;
 
-public class MySql {
+public class MySql implements  DataServer{
+
+
+    // crating the string arrays and return email
+
+
+    @Override
+    public String[] getmail() {
+        return new String[]{"anujsingh@gmail.com" , "anilsingh@gmail.com" , "tushar@gmail.com"};
+    }
 }
