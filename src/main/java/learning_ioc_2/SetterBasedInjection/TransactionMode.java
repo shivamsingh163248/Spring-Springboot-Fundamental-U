c773993c58fb520d8e75941b6d46b5e5f9c62c44
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-04 13:46:03 +0530

    learning the setter method to set the property

--- src/main/java/learning_ioc_2/SetterBasedInjection/TransactionMode.java
+++ src/main/java/learning_ioc_2/SetterBasedInjection/TransactionMode.java
@@ -1,5 +1,8 @@
 package learning_ioc_2.SetterBasedInjection;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface TransactionMode {
 
     public void transeation(int amount) ;
--- src/main/java/learning_ioc_2/SetterBasedInjection/TransportationSender.java
+++ src/main/java/learning_ioc_2/SetterBasedInjection/TransportationSender.java
@@ -1,10 +1,24 @@
 package learning_ioc_2.SetterBasedInjection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TransportationSender {
 
     // using the setter method to set the dependency
     private TransactionMode mode ;
-
     // creating the setter method for the setting the property
 
+
+    @Autowired
+    public void setMode(@Qualifier("NEFT") TransactionMode mode) {
+        this.mode = mode;
+    }
+
+    // taking the input and the print all the details
+    public  void aboutTransactionDetail(int amount){
+        mode.transeation(amount);
+    }
 }
