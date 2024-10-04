0e7d1941e79819a49519920990fd53207fc42b55
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-05 01:27:57 +0530

    updating the values

--- src/main/java/learning_ioc_2/beanFeatureWorking/application.java
+++ src/main/java/learning_ioc_2/beanFeatureWorking/application.java
@@ -1,5 +1,7 @@
 package learning_ioc_2.beanFeatureWorking;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,15 @@ class student{
     student(teacher teacher1){
         this.teacher = teacher1 ;
     }
+
+    // creating the init and destroy method
+    public void init(){
+        System.out.println("this is the init method ");
+    }
+    public  void DistMethod(){
+        System.out.println("this is the destroy method");
+    }
+
 
 
 }
@@ -26,12 +37,12 @@ class teacher{
 class  appConfig {
 
     // creating to configure
-    @Bean
+    @Bean(name = "firstStudent" , initMethod = "init" , destroyMethod = "DistMethod")     // creating
     public student checkStudent(teacher teacher){
         return  new student(teacher) ;
     }
 
-    @Bean
+    @Bean(name = "teacherTest" )
     public  teacher teacher(){
         return new teacher() ;
     }
@@ -41,4 +52,23 @@ class  appConfig {
 }
 
 public class application {
+
+    public static void main(String[] args) {
+
+        // again creating the main function for the testing and check the all the bean
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(appConfig.class);
+        // creating the arrays and the testing the values
+        String[] check = applicationContext.getBeanDefinitionNames();
+        // creating the for each loop 
+        for (String num : check){
+            System.out.println(num);
+        }
+
+        // now check using the name of the bean
+        teacher teacher = (learning_ioc_2.beanFeatureWorking.teacher) applicationContext.getBean("teacherTest");
+        // noe printing the teacher about
+        teacher.about();
+    }
+    
+
 }
